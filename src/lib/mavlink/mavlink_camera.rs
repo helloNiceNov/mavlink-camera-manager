e52be1e7b345c030df398a882854144b616b4606
helloNiceNov <283631483@qq.com>
2025-02-24 11:58:37 +0800

    add start capture cmd 03-6

--- src/lib/mavlink/mavlink_camera.rs
+++ src/lib/mavlink/mavlink_camera.rs
@@ -9,8 +9,7 @@ use url::Url;
 use crate::{
     cli, mavlink::mavlink_camera_component::MavlinkCameraComponent,
     network::utils::get_visible_qgc_address, stream::manager as stream_manager,
-    video::types::VideoSourceType,
-    video_stream::types::VideoAndStreamInformation,
+    video::types::VideoSourceType,video_stream::types::VideoAndStreamInformation,
 };
 
 use super::{manager::Message, utils::*};
@@ -441,7 +440,7 @@ impl MavlinkCameraInner {
                 match stream_manager::get_jpeg_thumbnail_from_source(
                     source.clone(),
                     quality,
-                    target_height
+                    target_height,
                 )
                 .await
                 {
